54c608f755d9b19d3314592d07452042659d8217
WangRenjun <wangrenjun12@sina.com>
2017-01-09 14:52:09 +0800

    添加基于aspejct 纯注解风格的AOP

---
+++ src/main/java/org/wrj/allspring/version4/aop/pureannotation/ServiceExecuteLog.java
@@ -0,0 +1,46 @@
+package org.wrj.allspring.version4.aop.pureannotation;
+
+import org.apache.commons.lang3.StringUtils;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ServiceExecuteLog {
+
+	
+	//@After("execution(* org.wrj.allspring.version4.aop.pureannotation.PayService.payOrder(..))")
+	@After("execution (public * *(..))")
+	public void afterExecute(JoinPoint jp){
+		System.out.println("afterExecute payOrder()");
+	}
+	
+	
+	@Before("execution(* org.wrj.allspring.version4.aop.pureannotation.PayService.payOrder(..))")
+	public void beforeExecute(JoinPoint jp){
+		System.out.println("beforeExecute payOrder()");
+	}
+	
+	@Around("execution(* org.wrj.allspring.version4.aop.pureannotation.PayService.payOrder(..))")
+	public Object around(ProceedingJoinPoint jp) throws Throwable{
+		Object[] args = jp.getArgs();
+		System.out.println("around payOrder() before execute,args="+StringUtils.join(args,",") );
+		
+		Object obj = jp.proceed(args);
+		System.out.println("around payOrder() after execute ,result="+obj);
+		return obj;
+	}
+	
+	@AfterThrowing(value="execution(* org.wrj.allspring.version4.aop.pureannotation.PayService.payOrdreForException(..))",throwing = "thr")
+	public void afterThrowing(JoinPoint jp,Throwable thr){
+		System.out.println("afterThrowing payOrdreForException()");
+	}
+	
+	
+}
---
+++ src/test/java/org/wrj/allspring/version4/aop/pureannotation/PayServiceTest.java
@@ -0,0 +1,20 @@
+package org.wrj.allspring.version4.aop.pureannotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.wrj.allspring.version4.aop.pureannotation.PayService;
+
+public class PayServiceTest {
+
+	
+	public static void main(String[] args) {
+
+		ApplicationContext ac = new ClassPathXmlApplicationContext(
+				"spring/aop/pureannotation/spring-config-pureannotation.xml");
+		PayService ps = ac.getBean("payService", PayService.class);
+		ps.payOrder("ZSYH0001", "20161105P01");
+		//ps.payOrdreForException();
+		
+		ps.payOrdreForException();
+	}
+}
